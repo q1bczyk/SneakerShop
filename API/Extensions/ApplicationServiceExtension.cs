c7ce28e7f67f51503bf7775fed2f78290da602b4
q1bczyk <bartekkubik7@gmail.com>
2024-03-13 15:55:34 +0100

    error handling added

---
+++ API/Extensions/ApplicationServiceExtension.cs
@@ -0,0 +1,10 @@
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtension
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services, IConfiguration config)
+        {
+            
+        }
+    }
+}
